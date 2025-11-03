52889cb625ea58dfa7237c8ff7a2ef0b16b386f2
동준영 <pioneer-d@naver.com>
2025-11-03 16:52:18 +0900

    [25.11.03] introduction:
    치킨 쿠폰 문제 Done

---
+++ app/src/main/java/com/pioneer/algorithm/introduction/ChickenCoupon.kt
@@ -0,0 +1,27 @@
+package com.pioneer.algorithm.introduction
+
+fun main(){
+    println(chickenCoupon(100))
+}
+
+fun chickenCoupon(chicken: Int): Int {
+    /**
+     * 1081 / 10 -> 108
+     * (108 + 1(나머지)) / 10 -> 10
+     * (10 + 9(나머지)) / 10 -> 1
+     * (1 + 9(나머지)) / 10 -> 1
+     * => 120
+     * */
+
+    var answer = 0
+    var temp = chicken
+
+    var breakPoint = true
+    while (breakPoint){
+        answer += temp / 10
+        temp = (temp / 10) + (temp % 10)
+        if (temp / 10 < 1) breakPoint = false
+    }
+
+    return answer
+}
