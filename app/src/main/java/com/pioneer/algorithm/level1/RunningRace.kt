26aef5b3d9b25ee8dedaf3c9c970b435adb54e52
동준영 <pioneer-d@naver.com>
2025-11-05 09:46:47 +0900

    [25.11.05] level1:
    달리기 경주 문제 Done
    시간 복잡도에 대한 개념의 시초!

---
+++ app/src/main/java/com/pioneer/algorithm/level1/RunningRace.kt
@@ -0,0 +1,80 @@
+package com.pioneer.algorithm.level1
+
+fun main(){
+    runningRace(arrayOf("mumu", "soe", "poe", "kai", "mine"), arrayOf("kai", "kai", "mine", "mine")).apply {
+        repeat(this.size){
+            print("${this[it] }"+" ")
+        }
+    }
+}
+
+fun runningRace(players: Array<String>, callings: Array<String>): List<String> {
+    val keyIsName = mutableMapOf<String, Int>()
+    for (i in 0 until players.size){
+        keyIsName[players[i]] = i
+    }
+
+    val keyIsRank = mutableMapOf<Int, String>()
+    for (i in 0 until players.size){
+        keyIsRank[i] = players[i]
+    }
+
+    repeat(callings.size){ index ->
+        val targetName = callings[index]
+        val targetCurrentRank = keyIsName[targetName]!!
+        val downName = keyIsRank[targetCurrentRank-1]!!
+        val downCurrentRank = keyIsName[downName]!!
+
+        keyIsRank[targetCurrentRank] = downName
+        keyIsRank[downCurrentRank] = targetName
+
+        keyIsName[targetName] = downCurrentRank
+        keyIsName[downName] = targetCurrentRank
+    }
+
+
+    val returnList: List<String> = MutableList(players.size) { "" }.apply {
+        for (i in players.indices) {
+            val index = keyIsName[players[i]] ?: continue
+            this[index] = players[i]
+        }
+    }
+
+    return returnList
+}
+
+//["mumu", "soe", "poe", "kai", "mine"]
+
+//["kai", "kai", "mine", "mine"]
+
+//["mumu", "kai", "mine", "soe", "poe"]
+
+
+//func solution(_ players:[String], _ callings:[String]) -> [String] {
+//    var temp = Dictionary<String, Int>()
+//    var players = players
+//    players.enumerated().map{temp[$1] = $0}
+//
+//    for c in callings {
+//        guard let i = temp[c] else { return [] }
+//        players.swapAt(i, i - 1)
+//
+//        temp[c]! -= 1
+//        temp[players[i]]! += 1
+//    }
+//
+//    return players
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
