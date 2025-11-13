a92409f70408d50a3ee853a7a4dae13eaf2f2d8e
동준영 <pioneer-d@naver.com>
2025-11-13 11:49:28 +0900

    [25.11.13] basic:
    Day 22 Done
    107번 문제 다시 풀어볼 것.

--- app/src/main/java/com/pioneer/algorithm/basic/Day22.kt
+++ app/src/main/java/com/pioneer/algorithm/basic/Day22.kt
@@ -1,10 +1,24 @@
 package com.pioneer.algorithm.basic
 
+import kotlin.math.absoluteValue
 
 fun main(){
-
+//    println(solution107("18446744073709551615", "287346502836570928366"))
+    println(solution110("banana", "wxyz"))
+//    solution("programmers").apply {
+//        repeat(this.size){
+//            print("${this[it]} ")
+//        }
+//    }
 }
-fun againSolution107(a: String, b: String): String {
+
+/** 0 떼기 */
+fun solution106(n_str: String): String {
+    return n_str.toInt().toString()
+}
+
+/** 두 수의 합 */
+fun solution107(a: String, b: String): String {
     var answer = ""
 //    val repeatCount = if (a.length < b.length) a.length else b.length
 //    var isUp = false
@@ -64,5 +78,31 @@ fun againSolution107(a: String, b: String): String {
     }
 
     if (isUp) "1"+answer
+    return answer
+}
+
+/** 문자열로 변환 */
+fun solution108(n: Int): String {
+    return n.toString()
+}
+
+/** 배열의 원소 삭제하기 */
+fun solution109(arr: IntArray, delete_list: IntArray): IntArray {
+    val answerSet = arr.toMutableSet()
+    repeat(delete_list.size){
+        answerSet.remove(delete_list[it])
+    }
+    return answerSet.toIntArray()
+}
+
+/** 부분 문자열인지 확인하기 */
+fun solution110(my_string: String, target: String): Int {
+    var answer = 0
+    for (i in 0 until my_string.length - target.length+1){
+        if (my_string.substring(i, i+target.length) == target) {
+            answer = 1
+            break
+        }
+    }
     return answer
 }
